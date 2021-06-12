a1ee548b7fe8d5aea2019e2ef83d7829e9a62f03
hankyul <consistant1y@ajou.ac.kr>
2021-06-12 14:33:42 +0900

    test: add test report
    two artifacts will be made
    1. coverage
    2. test result

--- findU/jest.config.js
+++ findU/jest.config.js
@@ -9,10 +9,25 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest',
   },
+
+  coverageDirectory: '../output/coverage/',
+  coverageReporters: ['text-summary', 'html'],
+
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue',
   ],
   setupFiles: ['./test/setup.js'], // <- add this
+
+  reporters: [
+    'default',
+    [
+      './node_modules/jest-html-reporter',
+      {
+        pageTitle: 'Test Report',
+        outputPath: '../output/test/test-report.html',
+      },
+    ],
+  ],
 }
